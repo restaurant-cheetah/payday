31eec854f595bd9b709761f33e2d1338be3be0bf
katiaAlexandrov <katia@restaurantcheetah.com>
2019-02-06 13:07:07 +0200

    Payday - changing fonts to ttf fonts that supports languages

--- lib/payday/prawn_ttf_document.rb
+++ lib/payday/prawn_ttf_document.rb
@@ -1,19 +1,31 @@
 module Payday
   class PrawnTtfDocument < Prawn::Document
+    @@fonts_status = nil
 
     def initialize(*args)
       super
       font_setup
     end
 
+    def self.fonts_status
+      @@fonts_status
+    end
+
     def font_setup
-      self.font_families.update("OpenSans" => {
-       :normal => File.join("lib/assets/fonts/OpenSans-Regular.ttf"),
-       :italic => File.join("lib/assets/fonts/OpenSans-Italic.ttf"),
-       :bold => File.join("lib/assets/fonts/OpenSans-Bold.ttf"),
-       :bold_italic => File.join("lib/assets/fonts/OpenSans-Bold-Italic.ttf")
-      })
-      self.font("OpenSans")
+      begin
+        fonts_path = File.join(File.dirname(__FILE__), "..", "..", "lib", "assets", "fonts")
+        self.font_families.update("OpenSans" => {
+         :normal => File.join(fonts_path,"OpenSans-Regular.ttf"),
+         :italic => File.join(fonts_path,"OpenSans-Italic.ttf"),
+         :bold => File.join(fonts_path,"OpenSans-Bold.ttf"),
+         :bold_italic => File.join(fonts_path,"OpenSans-Bold-Italic.ttf")
+        })
+        font("OpenSans")
+        @@fonts_status ||= 'ttf'
+      rescue  => e
+        @@fonts_status ||= 'default'
+      end
+
     end
   end
 end
